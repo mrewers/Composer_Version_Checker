03c101c2ff9f26392d0e75805146297cb5f9e7d1
Marek Rewers <mrewers@users.noreply.github.com>
2018-08-18 23:56:47 -0400

    Add toggle for admin menu

--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { toggleAdmin } from '../../utils/adminToggle';
+
 const Selector = () => (
   <div className="site-selector">
     <select className="site-drop-down">
       <option value="">Select A Site</option>
     </select>
-    <button type="button">Add Another Site</button>
+    <button id="admin-toggle" type="button" onClick={toggleAdmin}>Add Another Site</button>
   </div>
 );
 
---
+++ src/utils/adminToggle.js
@@ -0,0 +1,11 @@
+export function toggleAdmin() {
+  const adminMenu = document.querySelector('.admin-popup')
+  adminMenu.style.display = "flex";
+}
+
+// const adminToggle = document.getElementById('#admin-toggle');
+
+// adminToggle.addEventListener('click', e => {
+
+
+// })
