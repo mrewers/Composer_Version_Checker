4df9347f1e070900bb7181a12c2721d8cd2544a9
Marek Rewers <mrewers@users.noreply.github.com>
2018-08-21 22:43:11 -0400

    Style selector component

--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -6,11 +6,8 @@ import { toggleAdmin } from '../../utils/adminToggle';
 const Selector = ({ value, callback, getVersion }) => (
   <div className="site-selector">
     <select id="site-drop-down" value={value} className="site-drop-down" onChange={callback} />
-    <h1>WordPress Version Checker</h1>
-    <div>
-      <button id="admin-toggle" type="button" onClick={toggleAdmin}>Add Another Site</button>
-      <button id="version-toggle" type="button" onClick={getVersion}>Update Versions</button>
-    </div>
+    <button id="admin-toggle" type="button" onClick={toggleAdmin}>Add Another Site</button>
+    <button id="version-toggle" type="button" onClick={getVersion}>Update Versions</button>
   </div>
 );
 
